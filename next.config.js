1975b1b5b5fdf85e204dec78219aa306f74e37d9
Gumilang Wahyu Khairuddin <84616972+gumilangwahyu@users.noreply.github.com>
2026-01-19 15:27:28 +0700

    Create next.config.js with Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    reactStrictMode: true,
+    // Additional configurations can be added here
+};
+
+module.exports = nextConfig;
